70c2f4d361fe665468421a9b8641837bbe03177d
PoojaLloyds <pooja1978@gmail.com>
2023-06-16 12:54:49 +0100

    Revert "updated the comments i the begining  of lbg.py"
    This reverts commit 64acfd7f88b8fde19c040268163264b0c8811a67.

--- lbg.py
+++ lbg.py
@@ -5,8 +5,6 @@ NB. mixes LBYL and EAFP to reflect simpler logic
 This version using simple prints to STDOUT rather than formalised logger
 """
 
-#This is change to the file  to learn
-
 # import argparse for passing PORT variable through the cli
 import argparse
 
